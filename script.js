17d092705724acb05d0c4a71bd0ac72d7d798a4a
Alysson Henrique <alyssonhra99@gmail.com>
2023-03-29 23:15:22 -0300

    all-done + bonus - before testing

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ const selectOption = (e) => {
     for (let i = 0; i < [...divPai.children].length; i++) {
       if (divPai.children[i].id === divSelecionada.id) {
         divSelecionada.classList.add("selected");
+
         // Fazendo o ion-icon de "ok" aparecer:
         divSelecionada.children[2].classList.add("visible");
 
@@ -43,9 +44,12 @@ const selectOption = (e) => {
     }
   }
 
-  const orderConfirmation = (e) => {
+  const orderConfirmation = () => {
     document.getElementsByClassName('container')[0].style.opacity = 0.2;
     
+    const name = prompt("Qual seu nome?");
+    const adress = prompt("Qual seu endereço?");
+
     const confirmation = document.getElementById('confirmation');
     confirmation.classList.remove('not-visible');
     confirmation.classList.add('confirmation-panel');
@@ -57,6 +61,31 @@ const selectOption = (e) => {
         confirmation.children[i + 1].getElementsByTagName('span')[0]
             .textContent = pricesSelection[i].toString().replaceAll(".", ",");
     }
+
+    // Capturando o botão da finalização do pedido e encaminhamento para o wpp ou cancelamento:
+    const finishBtn = document.getElementById('deal');
+    const cancelBtn = document.getElementById('cancel');
+
+    // Encaminha para o whats em caso de pedido finalizado:
+    finishBtn.addEventListener('click', (e) => {
+        location.href = `https://wa.me/5542999009040?text=${encodeURIComponent(`
+        Olá, gostaria de fazer o pedido:
+        - Prato: Frango Yin Yang
+        - Bebida: Coquinha Gelada
+        - Sobremesa: Pudim
+        Total: R$ ${total.toFixed(2).toString().replaceAll('.', ',')}
+        
+        Nome: ${name}
+        Endereço: ${adress}
+        `)}`;
+    });
+
+    // Cancelando o pedido e voltando a tela de seleção:
+    cancelBtn.addEventListener('click', () => {
+        document.getElementsByClassName('container')[0].style.opacity = 1;
+        confirmation.classList.add('not-visible');
+        confirmation.classList.remove('confirmation-panel');
+    });
   }
 
   // Se as 3 opções forem selecionadas, o botão de fechar pedido é liberado:
@@ -64,11 +93,12 @@ const selectOption = (e) => {
   if ([...document.getElementsByClassName("selected")].length === 3) {
 
     const button = document.getElementById("checkout");
+    button.disabled = false;
     button.classList.add("available");
     button.children[0].textContent = "Fechar Pedido";
     total = meal + drink + dessert;    
 
-    button.addEventListener('click', (e) => orderConfirmation(e));
+    button.addEventListener('click', () => orderConfirmation());
   }
 };
 
@@ -76,9 +106,7 @@ const selectOption = (e) => {
 // e sobremesa. De forma que se mais opções forem disponibilizadas, estas automaticamente ganham um id única.
 
 for (let i = 0; i < divMeal.length; i++) {
-  divMeal.item(i).id = `meal-${i}`;
-  // De forma análoga, é adicionada uma id única para o preço de cada uma das opções:
-  prices.item(i).id = `meal-price-${i}`;
+  divMeal.item(i).id = `meal-${i}`; 
 
   // Adicionando um event listener para click, em cada div:
   divMeal.item(i).addEventListener("click", (e) => selectOption(e));
@@ -86,8 +114,6 @@ for (let i = 0; i < divMeal.length; i++) {
 
 for (let i = 0; i < divDrink.length; i++) {
   divDrink.item(i).id = `drink-${i}`;
-  // De forma análoga, é adicionada uma id única para o preço de cada uma das opções:
-  prices.item(i + divMeal.length).id = `meal-price-${i + divMeal.length}`;
 
   // Adicionando um event listener para click, em cada div:
   divDrink.item(i).addEventListener("click", (e) => selectOption(e));
@@ -95,10 +121,6 @@ for (let i = 0; i < divDrink.length; i++) {
 
 for (let i = 0; i < divDessert.length; i++) {
   divDessert.item(i).id = `dessert-${i}`;
-  // De forma análoga, é adicionada uma id única para o preço de cada uma das opções:
-  prices.item(i + divMeal.length + divDrink.length).id = `meal-price-${
-    i + divMeal.length + divDrink.length
-  }`;
 
   // Adicionando um event listener para click, em cada div:
   divDessert.item(i).addEventListener("click", (e) => selectOption(e));
