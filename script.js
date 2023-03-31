02b176d1fdbfec6f379cadf6167efb99d74d22fe
Alysson Henrique <alyssonhra99@gmail.com>
2023-03-31 00:00:52 -0300

    cancel-button bug fixed

--- script.js
+++ script.js
@@ -2,13 +2,59 @@
 const divMeal = document.getElementsByClassName("meal");
 const divDrink = document.getElementsByClassName("drink");
 const divDessert = document.getElementsByClassName("dessert");
-const prices = document.getElementsByClassName("price");
 
 // Variaveis para armazenar o preço das opções:
 let meal = 0;
 let drink = 0;
 let dessert = 0;
 let total = 0;
+
+const orderConfirmation = (e) => {
+  e.currentTarget.disabled = true;
+
+  document.querySelector(".container").style.opacity = 0.2;
+
+  const name = prompt("Qual seu nome?");
+  const adress = prompt("Qual seu endereço?");
+
+  const confirmation = document.getElementById("confirmation");
+  confirmation.classList.remove("not-visible");
+  confirmation.classList.add("confirmation-panel");
+
+  const pricesSelection = [meal, drink, dessert, total.toFixed(2)];
+
+  // Adicionando o valor dos pedidos e o valor total no painel de confirmação:
+  for (let i = 0; i < confirmation.children.length - 3; i++) {
+    confirmation.children[i + 1].getElementsByTagName("span")[0].textContent =
+      pricesSelection[i].toString().replaceAll(".", ",");
+  }
+
+  // Capturando o botão da finalização do pedido e encaminhamento para o wpp ou cancelamento:
+  const finishBtn = document.getElementById("deal");
+  const cancelBtn = document.getElementById("cancel");
+
+  // Encaminha para o whats em caso de pedido finalizado:
+  finishBtn.addEventListener("click", () => {
+    location.href = `https://wa.me/5542999009040?text=${encodeURIComponent(`
+      Olá, gostaria de fazer o pedido:
+      - Prato: Frango Yin Yang
+      - Bebida: Coquinha Gelada
+      - Sobremesa: Pudim
+      Total: R$ ${total.toFixed(2).toString().replaceAll(".", ",")}
+      
+      Nome: ${name}
+      Endereço: ${adress}
+      `)}`;
+  });
+
+  // Cancelando o pedido e voltando a tela de seleção:
+  cancelBtn.addEventListener("click", () => {
+    document.getElementById("checkout").disabled = false;
+    document.querySelector(".container").style.opacity = 1;
+    confirmation.classList.add("not-visible");
+    confirmation.classList.remove("confirmation-panel");
+  });
+};
 
 // Função para alterar o estilo da opção selecionada e armazenar o preço da mesma:
 const selectOption = (e) => {
@@ -24,99 +70,44 @@ const selectOption = (e) => {
         divSelecionada.children[2].classList.add("visible");
 
         // Capturando o preço da opção selecionada (melhorando o commit final e elimando repetições de código):
-        const priceOption = (element) => { 
-
-            return element.getElementsByClassName("price")[0]
-                .textContent.replaceAll(",", "."); 
-        
-        }
+        const priceOption = (element) => {
+          return element
+            .getElementsByClassName("price")[0]
+            .textContent.replaceAll(",", ".");
+        };
 
         // Armazenando o preço da opção:
         if (divSelecionada.id.includes("meal")) {
-
           meal = +priceOption(divSelecionada);
-
         } else if (divSelecionada.id.includes("drink")) {
-
           drink = +priceOption(divSelecionada);
-
         } else if (divSelecionada.id.includes("dessert")) {
-
           dessert = +priceOption(divSelecionada);
-
         }
       } else {
-
         divPai.children[i].classList.remove("selected");
         divPai.children[i].children[2].classList.remove("visible");
-        
       }
     }
   }
 
-  const orderConfirmation = () => {
-    document.getElementsByClassName('container')[0].style.opacity = 0.2;
-    
-    const name = prompt("Qual seu nome?");
-    const adress = prompt("Qual seu endereço?");
-
-    const confirmation = document.getElementById('confirmation');
-    confirmation.classList.remove('not-visible');
-    confirmation.classList.add('confirmation-panel');
-
-    const pricesSelection = [meal, drink, dessert, total.toFixed(2)];
-
-    // Adicionando o valor dos pedidos e o valor total no painel de confirmação:
-    for (let i = 0; i < confirmation.children.length - 3; i++) {
-        confirmation.children[i + 1].getElementsByTagName('span')[0]
-            .textContent = pricesSelection[i].toString().replaceAll(".", ",");
-    }
-
-    // Capturando o botão da finalização do pedido e encaminhamento para o wpp ou cancelamento:
-    const finishBtn = document.getElementById('deal');
-    const cancelBtn = document.getElementById('cancel');
-
-    // Encaminha para o whats em caso de pedido finalizado:
-    finishBtn.addEventListener('click', (e) => {
-        location.href = `https://wa.me/5542999009040?text=${encodeURIComponent(`
-        Olá, gostaria de fazer o pedido:
-        - Prato: Frango Yin Yang
-        - Bebida: Coquinha Gelada
-        - Sobremesa: Pudim
-        Total: R$ ${total.toFixed(2).toString().replaceAll('.', ',')}
-        
-        Nome: ${name}
-        Endereço: ${adress}
-        `)}`;
-    });
-
-    // Cancelando o pedido e voltando a tela de seleção:
-    cancelBtn.addEventListener('click', () => {
-        document.getElementsByClassName('container')[0].style.opacity = 1;
-        confirmation.classList.add('not-visible');
-        confirmation.classList.remove('confirmation-panel');
-    });
-  }
-
   // Se as 3 opções forem selecionadas, o botão de fechar pedido é liberado:
-
   if ([...document.getElementsByClassName("selected")].length === 3) {
-
-    const button = document.getElementById("checkout");
     button.disabled = false;
     button.classList.add("available");
     button.children[0].textContent = "Fechar Pedido";
-    total = meal + drink + dessert;    
-
-    button.addEventListener('click', () => orderConfirmation());
+    total = meal + drink + dessert;
   }
 };
 
-// Com as div's, agora será implementado, dinamicament uma id para cada uma das opções referente ao prato, bebida
-// e sobremesa. De forma que se mais opções forem disponibilizadas, estas automaticamente ganham um id única.
+// Passo 1:
+// Com as div's armazenadas nas variáveis declaradas nas primeiras linhas, agora será implementado,
+// dinamicament uma id para cada uma das opções referente ao prato, bebida
+// e sobremesa. De forma que se mais opções forem disponibilizadas,
+// estas automaticamente ganham um id única.
 
 for (let i = 0; i < divMeal.length; i++) {
-  divMeal.item(i).id = `meal-${i}`; 
+  divMeal.item(i).id = `meal-${i}`;
 
   // Adicionando um event listener para click, em cada div:
   divMeal.item(i).addEventListener("click", (e) => selectOption(e));
@@ -135,3 +126,14 @@ for (let i = 0; i < divDessert.length; i++) {
   // Adicionando um event listener para click, em cada div:
   divDessert.item(i).addEventListener("click", (e) => selectOption(e));
 }
+
+// Passo 2:
+// Adicionando a função de fechar o pedido quando o botão fixo da parte de baixo for clicado,
+// lembrando que inicialmente ele está desabilitado (no html), sendo habilitado apenas quando 3
+// opções forem selecionadas:
+
+const button = document.getElementById("checkout");
+button.addEventListener("click", (e) => orderConfirmation(e));
+
+// Passo 3:
+// Executar as funções adicionadas aos eventos de click, de acordo com o oque o usuario fizer
